c05b6020107c7386dfeca29d5e9d9cb587d24034
Todd Channick <toc5012@gmail.com>
2016-03-03 14:43:41 -0500

    Ball movement

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,13 @@
 var canvas;
-var	context;
+var context;
 // width and height of canvas
 var W = 650,
-		H = 500;
+	H = 500;
 
 // Initialize the game canvas
-function init(){
+function init() {
 	canvas = document.getElementById('game-canvas');
-	context = canvas.getContext('2d');	
+	context = canvas.getContext('2d');
 	canvas.width = W;
 	canvas.height = H;
 }
@@ -21,9 +21,9 @@ function paintCanvas() {
 // Draws the game board (boundaries)
 function drawBoundaries() {
 	context.beginPath();
-	context.lineWidth = 6;
+	context.lineWidth = 4;
 	context.strokeStyle = 'black';
-	context.strokeRect(25, 25, 600, 450);
+	context.strokeRect(20, 20, 610, 460);
 }
 
 function addKeyEvents() {
@@ -38,41 +38,50 @@ function Paddle(x, y) {
 	this.width = 15;
 	this.height = 100;
 	this.speed = 10;
-	// Move the vertical distance 'dy'
-	this.move = function (dy) {
-		// clear the current rectangle
-		context.clearRect(this.x, this.y, this.width, this.height);
-		this.y += dy;
-	}
-	this.render = function () {
-		context.beginPath();
-		context.fillStyle = this.color;
-		context.fillRect(this.x, this.y, this.width, this.height);
-	};
+}
+
+// Move the vertical distance 'dy'
+Paddle.prototype.move = function (dy) {
+	// clear the current rectangle
+	context.clearRect(this.x, this.y, this.width, this.height);
+	this.y += dy;
+}
+
+Paddle.prototype.render = function () {
+	context.beginPath();
+	context.fillStyle = this.color;
+	context.fillRect(this.x, this.y, this.width, this.height);
 }
 
 function onKeyDown(e) {
 	// up arrow
 	if (e.keyCode === 38) {
 		// Move 10 pixels on the y axis towards the origin
-		// Only if the paddle y position equals 35 or more pixels (still has room to move 10 pixels to top boundary)
-		if (human.y >= 35){
-			human.move(-10);	
+		// Only if the paddle y position equals 30 or more pixels (still has room to move 10 pixels to top boundary)
+		if (human.paddle.y >= 30) {
+			human.paddle.move(-10);
 		}
 	}
 	// down arrow
 	if (e.keyCode === 40) {
 		// Move 10 pixels on the y axis away from the origin 
-		// Only if the paddle y position equals 365 or fewer pixels (still has room to move 10 pixels to bottom boundary)
-		if (human.y <= 365){
-			human.move(10);
-		}	
+		// Only if the paddle y position equals 370 or fewer pixels (still has room to move 10 pixels to bottom boundary)
+		if (human.paddle.y <= 370) {
+			human.paddle.move(10);
+		}
 	}
 }
 
-// Declaring a human and computer paddle
-var human = new Paddle(35, 200);
-var computer = new Paddle(600, 200);
+function Human() {
+	this.paddle = new Paddle(30, 200);
+}
+
+function Computer() {
+	this.paddle = new Paddle(605, 200);
+}
+
+var human = new Human();
+var computer = new Computer();
 
 
 // Ball Object Constructor
@@ -81,31 +90,44 @@ function Ball() {
 	this.y = 250;
 	this.color = '#AC4E7D';
 	this.radius = 10;
-	this.render = function () {
-		context.fillStyle = this.color;
-		context.beginPath();
-		context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
-		context.fill();
-	};
+	this.velocity_x = 5;
+	this.velocity_y = 0;
 }
 
-// Declaring a new ball
+Ball.prototype.render = function () {
+	context.fillStyle = this.color;
+	context.beginPath();
+	context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+	context.fill();
+};
+
+// Build a new ball
 var ball = new Ball();
+
+Ball.prototype.update = function() {
+	this.x += this.velocity_x;
+	this.y += this.velocity_y;
+};
+
+var update = function () {
+	ball.update();
+};
 
 var render = function () {
 	paintCanvas();
+	human.paddle.render();
+	computer.paddle.render();
 	drawBoundaries();
-	human.render();
-	computer.render();
 	ball.render();
-	// redraw
-	animate(render);
 };
 
+
 // Actions needed for each repaint
-function step(){
+var step = function () {
+	update();
 	render();
-}
+	animate(step);
+};
 
 // Animate function
 var animate = window.requestAnimationFrame ||
